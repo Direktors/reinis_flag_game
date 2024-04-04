2e389506f4de15f95a8ec408b4a1d5d65f7d7685
Direktors <153750972+Direktors@users.noreply.github.com>
2024-04-04 20:36:09 +0300

    Add files via upload
    The str_xor function does not need to be reverse engineered for this challenge.

---
+++ Reinis_capture_falg/script.js
@@ -0,0 +1,13 @@
+// Get the buttons
+const homeButton = document.getElementById('homeButton');
+const aboutButton = document.getElementById('aboutButton');
+
+// Add click event listeners to the buttons 
+homeButton.addEventListener('click', function() {
+  window.location.href = 'index.html';
+});
+
+aboutButton.addEventListener('click', function() {
+  window.location.href = 'about.html';
+});
+// ( <!-- Reinis is forever. Anyways have 2/3 of the flag: FmZ3UzdDBxM-->)
